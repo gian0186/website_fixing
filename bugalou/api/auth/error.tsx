30db6d2d377c664411fb41b4e9ba411602c3c2d0
Gian Boender <gianboender@outlook.com>
2025-11-29 21:09:56 +0100

    changes

---
+++ bugalou/api/auth/error.tsx
@@ -0,0 +1,9 @@
+// app/api/auth/error.tsx
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>Er is een fout opgetreden tijdens de inlog.</h1>
+      <p>Probeer het opnieuw of neem contact op met de support.</p>
+    </div>
+  );
+}
